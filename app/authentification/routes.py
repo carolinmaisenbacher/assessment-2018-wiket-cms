8b19549a2d4b1262287023dab5e08fb7c30d875a
carolinmaisenbacher <carolin.maisenbacher@code.berlin>
2018-10-09 15:26:37 +0200

    finished basic functional tests of signup function

--- app/authentification/routes.py
+++ app/authentification/routes.py
@@ -3,7 +3,7 @@ from flask import request, abort, jsonify, make_response
 import bcrypt
 
 users = []   
-
+id_counter = 0
 
 @blueprint.route("/signup", methods=["POST"])
 def create_user():
@@ -12,22 +12,19 @@ def create_user():
     email = request.json['email']
     if not email:
         return make_response(jsonify({"error": "You have to provide a valid Email"}), 401)
-    
-    # check if user exists already
-    # isn't that an easy way to find out as a hacker whether an email exists already
+
     if User.find(email):
         return make_response(jsonify({"error": "You already exist, please log in"}), 401)
 
-    #if not create new user
     password = request.json['password']
    
     if not password:
         return make_response(jsonify({"error": "You have to provide a Password"}), 401)
-     
+
     new_user = User(email, password)
     users.append(new_user)
 
-    return make_response(jsonify({"userId": new_user.email}), 201)
+    return make_response(jsonify({"userId": new_user.id}), 201)
 
 
 
@@ -37,27 +34,29 @@ def login():
         abort(400)
     password = request.json['password']
 
-    user = None
-     # check if user exists
-    for saved_user in users:
-        if saved_user.email == request.json['email']:
-            user = saved_user
-
-    if user == None:
+    if not User.find(email):
         return make_response(jsonify({"error": "Username or passowrd incorrect!"}), 401)
 
-    # check it correct password
     if user.check_password(password):
-        return make_response(jsonify({"userId" : user.email}), 200) 
+        return make_response(jsonify({"userId" : user.userId}), 200) 
 
     else:  
         return make_response(jsonify({"error": "Username or password incorrect"}), 401)
 
+
+
 class User ():
     def __init__(self, email, password):
+        self.id = self.generate_id()
         self.email = email
         self.hashed_password = ""
         self.set_password(password)
+    
+    def generate_id(self):
+        global id_counter
+        id_counter += 1
+        return id_counter
+        
 
     def check_password(self, password):
         return bcrypt.checkpw(password.encode(), self.hashed_password)
@@ -70,6 +69,7 @@ class User ():
         for user in users:
             if user.email == email:
                 return user
+
 
 
 
--- app/authentification/tests.py
+++ app/authentification/tests.py
@@ -16,56 +16,81 @@ class SignUpCase(unittest.TestCase):
        self.hostname = "http://127.0.0.1:5000/"
        self.app = create_app(TestConfig)
        self.headers = {'content-type': 'application/json'}
+       routes.users = []
 
     def tearDown(self):
-        routes.users = []
-
+        pass
 
     # when a new user successfully signs up with a email, and a password the userid should be returned and the status code should be 201
     def test_valid_sign_up(self):     
-        data = json.dumps({'email' : 'example2@mail.com', 'password': 'paspythosword'})
+        email = 'example2@mail.com'
+        password = 'paspythosword'
+        data = {'email' : email, 'password': password}
         
-        with self.app.test_client() as c:
-            result = c.post(self.hostname + 'auth/signup', headers=self.headers, data=data)
-        
+        result = self.post_to_signup(data)
+        user = routes.User.find(email)
+
         self.assertEqual(result.status_code, 201, "User was not created after a json object with email and password was posted")
-        self.assertEqual([*result.json], ["userId"], "Successful sign up does not return the userId")
+        self.assertEqual(result.json, {"userId" : user.id}, "Successful sign up does not return the correct userId")
+        self.assertTrue(user, "User was not saved after signup")
+        self.assertEqual(user.email, email)
+        self.assertNotEqual(user.hashed_password, password, "Password of user is not hashed.")
+        # ???? Would I test, if the passwor is correctly hashed? I feel like that would be more than just testing the functionality.
+        self.assertTrue(user.id)
+
+
+        
+  
+
         
 
+
+
     
+    def test_request_with_incomplete_json(self):
+        data = {'email': 'paspythosword'}
+        data = {'password': 'paspythosword'}
+        data = {}
+        
+        result_just_email = self.post_to_signup(data)
+        result_just_password = self.post_to_signup(data)
+        result_empty_json = self.post_to_signup(data)
+        
+        self.assertEqual(result_just_email.status_code, 400)
+        self.assertEqual(result_just_password.status_code, 400)
+        self.assertEqual(result_empty_json.status_code, 400)
+
+
     # if a user wants to sign up with a username that is already taken, a 400 should be returned saying, that username is already taken
     def test_signup_with_already_used_email(self):
-        data = json.dumps({'email' : 'example2@mail.com', 'password': 'paspythosword'})
+        data = {'email' : 'example2@mail.com', 'password': 'paspythosword'}
         
-        with self.app.test_client() as c:
-            result1 = c.post(self.hostname + 'auth/signup', headers=self.headers, data=data)
-            result2 = c.post(self.hostname + 'auth/signup', headers=self.headers, data=data)
+        result1 = result = self.post_to_signup(data)
+        result2 = result = self.post_to_signup(data)
         
         self.assertEqual(result2.status_code, 401, "User was able to signup with an email that was used before")
         self.assertEqual([*result2.json], ["error"], "No error was raised when user tried to signup with a email that was used before")
 
     # a user needs to provide a password
     def test_signup_without_password(self):
-        data = json.dumps({'email' : 'example2@mail.com', 'password': ''})
+        data = {'email' : 'example2@mail.com', 'password': ''}
         
-        with self.app.test_client() as c:
-            result = c.post(self.hostname + 'auth/signup', headers=self.headers, data=data)
+        result = self.post_to_signup(data)
         
         self.assertEqual(result.status_code, 401, "User could sign up without providing a password")
         self.assertEqual([*result.json], ["error"], "On sign up, not providing a password did not raise an error")
 
     # a user needs to provide a email
     def test_signup_without_email(self):
-        data = json.dumps({'email' : '', 'password': '123'})
+        data = {'email' : '', 'password': '123'}
         
-        with self.app.test_client() as c:
-            result = c.post(self.hostname + 'auth/signup', headers=self.headers, data=data)
+        result = self.post_to_signup(data)
         
         self.assertEqual(result.status_code, 401, "User could sign up without providing an email")
         self.assertEqual([*result.json], ["error"], "On sign up, not providing an email did not raise an error")
 
     ###
-    # as user needs to provide a VALID email
+    ###### as user needs to provide a VALID email
     ###
 
     ## http get request is not allowed on /auth/signup
@@ -75,23 +100,24 @@ class SignUpCase(unittest.TestCase):
         with self.app.test_client() as c:
             result = c.get(self.hostname + 'auth/signup', headers=self.headers, data=data)
 
-        self.assertEqual(result.status_code, 405, "Get request on /auth/signup should not be allowed")
-
-    
+        self.assertEqual(result.status_code, 405, "Get request to /auth/signup should not be allowed")
 
     # after signup a user should be saved, with a userid
-    ## the users password need to be saved as a hash
-
-
-    # a user that signed up once should be able to log into the application when he returns to the side at some point later in time
     
+
+    ###
+    # a user that signed up once should be able to log into the application when he returns to the side at some point later in time
+    ###
 
     # the login function needs to be tested
     def test_finish(self):
         self.assertFalse(True, "finish the tests")
 
-
-
-
+    def post_to_signup(self, content):
+        json_content = json.dumps(content)
+        headers = {'content-type': 'application/json'}
+        with self.app.test_client() as c:
+            return c.post(self.hostname + 'auth/signup', headers=headers, data=json_content)
+    
 if __name__ == '__main__':
     unittest.main()     
