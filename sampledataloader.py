92ac5719bd603a855c3910098a04d1c2ffd9b0b6
carolinmaisenbacher <carolin.maisenbacher@code.berlin>
2018-11-04 22:16:53 +0100

    pledataloader loads owner data
    Signed-off-by: carolinmaisenbacher <carolin.maisenbacher@code.berlin>

---
+++ sampledataloader.py
@@ -0,0 +1,17 @@
+from app.config import Config
+from sqlalchemy.sql import text
+import psycopg2
+
+Config.SQLALCHEMY_DATABASE_URI
+connection = psycopg2.connect(dbname=Config.DATABASE_NAME, host="localhost", port=5432, user=Config.DATABASE_USER, password=Config.DATABASE_SECRET)
+
+datasets = []
+
+with open('dataset_user.csv', 'rb') as f:
+    cursor = connection.cursor()
+    cmd = 'COPY owner(id,first_name,last_name,email,password_hash) FROM STDIN WITH (FORMAT CSV, HEADER)'
+    cursor.copy_expert(cmd, f)
+    connection.commit()
+
+connection.close()
+
